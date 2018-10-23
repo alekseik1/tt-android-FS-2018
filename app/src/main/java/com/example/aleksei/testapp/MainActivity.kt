b036953221423ca30db6c81fc7b2845184445e57
alekseik1 <1alekseik1@gmail.com>
2018-10-23 20:21:57 +0300

    Сделал таймер и его сохранение между поворотами

--- app/src/main/java/com/example/aleksei/testapp/MainActivity.kt
+++ app/src/main/java/com/example/aleksei/testapp/MainActivity.kt
@@ -2,16 +2,63 @@ package com.example.aleksei.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.PersistableBundle
+import android.widget.Button
 import android.widget.TextView
+import java.sql.Time
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    var currTime = 0L
+    val BASIC_TIME = 10L
+
+    lateinit var timer: CountDownTimer
+
+    fun initTimer(totalTime: Long): CountDownTimer {
+        val timer = object: CountDownTimer(TimeUnit.SECONDS.toMillis(totalTime), TimeUnit.SECONDS.toMillis(1)) {
+
+            override fun onFinish() {
+                tvHello.text = "Закончили считать"
+            }
+
+            override fun onTick(value: Long) {
+                tvHello.text = getNaturalTime(value).toString()
+                currTime = TimeUnit.MILLISECONDS.toSeconds(value)
+            }
+        }
+        return timer
+    }
+
+    fun getNaturalTime(timeMillis: Long): Long {
+        val timeSeconds = TimeUnit.MILLISECONDS.toSeconds(timeMillis)
+        return timeSeconds
+    }
+
+    lateinit var tvHello: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvHello = findViewById<TextView>(R.id.tv_hello)
-        tvHello.text = "Hello, Kotlin!"
+        tvHello = findViewById(R.id.tv_hello)
 
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        btnStart.setOnClickListener {
+            var startTime = BASIC_TIME
+            savedInstanceState?.run {
+                startTime = getLong("currTime", BASIC_TIME)
+            }
+            timer = initTimer(startTime)
+            timer.start()
+        }
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putLong("currTime", currTime)
+        timer.cancel()
+        super.onSaveInstanceState(outState)
     }
 }
